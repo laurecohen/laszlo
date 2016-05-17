23c234b2e6b221a6c80ec0602e9dea8a0b2df113
Laure Cohen <laure.chn@gmail.com>
2016-05-17 19:03:17 +0200

    correction erreurs

---
+++ param-sample.js
@@ -0,0 +1,10 @@
+// param for twitter
+
+module.exports = {
+  consumer_key        : '****',
+  consumer_secret     : '****',
+  access_token        : '****',
+  access_token_secret : '****'
+};
+
+console.log("…Laszlo params ready");
